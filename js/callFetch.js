5d75460fe66200d7f740a34e85feb8dee3339948
Ntpsrm <155063980+Ntp-MD@users.noreply.github.com>
2024-08-03 16:31:02 +0700

    .

--- js/callFetch.js
+++ js/callFetch.js
@@ -1,8 +1,22 @@
 
-
+/*
 document.addEventListener("DOMContentLoaded", async () => {
     document.querySelectorAll('html *').forEach(async (element) => {
         const filePath = `https://ntp-md.github.io/local-demo/component/${element.id}.html`;
+        try {
+            const response = await fetch(filePath);
+            if (!response.ok) throw new Error('Failed to fetch');
+            element.innerHTML = await response.text();
+        } catch (error) {
+            console.error('Missing path', error);
+        }
+    });
+});
+*/
+
+document.addEventListener("DOMContentLoaded", async () => {
+    document.querySelectorAll('html *').forEach(async (element) => {
+        const filePath = `/component/${element.id}.html`;
         try {
             const response = await fetch(filePath);
             if (!response.ok) throw new Error('Failed to fetch');
